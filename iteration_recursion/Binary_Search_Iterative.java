b5a6bd35da6d3997adf6adb27245ab87d866db2a
Dr. Irshed Hussain <70743676+Irshed-Hussain@users.noreply.github.com>
2025-09-20 13:14:42 +0530

    Update Binary_Search_Iterative.java

--- iteration_recursion/Binary_Search_Iterative.java
+++ iteration_recursion/Binary_Search_Iterative.java
@@ -1,5 +1,5 @@
-public class Binary_Search_Iterative {
-    public static int search_algo(int[] A, int key, int p, int r) {
+public class Binary_Search {
+    public static int Search_Iterative(int[] A, int key, int p, int r) {
         while (p <= r) {
             int mid = (p+r) / 2; 
             if (A[mid] == key)
@@ -12,10 +12,19 @@ public class Binary_Search_Iterative {
         return -1;
     }
 
+    
+
     public static void main(String[] args) {
         int[] A = {1, 2, 3, 4, 5, 6};
         int key = 4, p = 0, r = A.length-1;
-        int idx = search_algo(A, key, p, r);
+        
+        int id = Search_Iterative(A, key, p, r);
+        if (id == -1)
+            System.out.println("Element " + key + " not found in the array!");
+        else
+            System.out.println("Element " + key + " found at index: " + id);
+        
+        int idx = Search_Recursive(A, key, p, r);
         if (idx == -1)
             System.out.println("Element " + key + " not found in the array!");
         else
