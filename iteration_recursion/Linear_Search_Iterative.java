aec8dcc0cf28b73239c07e73c364834d30f55f62
Dr. Irshed Hussain <70743676+Irshed-Hussain@users.noreply.github.com>
2025-09-20 12:57:27 +0530

    Update Linear_Search_Iterative.java

--- iteration_recursion/Linear_Search_Iterative.java
+++ iteration_recursion/Linear_Search_Iterative.java
@@ -1,19 +1,19 @@
-public class Linear_Search_Recursive {
-     public static int search_algo(int[] A, int key, int idx) {
-        if (idx > A.length-1)
-            return -1;
-        if (A[idx] == key)
-            return idx;
-        return search_algo(A, key, idx + 1);
-    }
+public class Linear_Search_Iterative {
+    public static int search_algo(int[] A, int key, int idx) {
+       for(int i=0; i<A.length-1; i++) {
+            if(A[i] == key)
+                return i;
+       }
+       return -1;
+   }
 
-    public static void main(String[] args) {
-        int[] A = {6, 2, 4, 5, 1, 3};
-        int key = 5;
-        int idx = search_algo(A, key, 0);
-        if (idx == -1)
-            System.out.println("Element " + key + " not found in the array!");
-        else
-            System.out.println("Element " + key + " found at index: " + idx);
-    }
+   public static void main(String[] args) {
+       int[] A = {6, 2, 4, 5, 1, 3};
+       int key = 5;
+       int idx = search_algo(A, key, 0);
+       if (idx == -1)
+           System.out.println("Element " + key + " not found in the array!");
+       else
+           System.out.println("Element " + key + " found at index: " + idx);
+   }
 }
