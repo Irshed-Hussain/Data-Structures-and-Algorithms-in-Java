14eeba065e549e8f364d72aa3317369bc6cd1e91
Dr. Irshed Hussain <70743676+Irshed-Hussain@users.noreply.github.com>
2025-09-20 13:16:37 +0530

    Update Binary_Search_Iterative.java

--- iteration_recursion/Binary_Search_Iterative.java
+++ iteration_recursion/Binary_Search_Iterative.java
@@ -12,7 +12,17 @@ public class Binary_Search {
         return -1;
     }
 
-    
+    public static int Search_Recursive(int[] A, int key, int p, int r) {
+        if (p > r)
+            return -1;
+        int mid = (p+r) / 2;
+        if (A[mid] == key)
+            return mid;
+        else if (A[mid] < key)
+            return Search_Recursive(A, key, mid+1, r);
+        else
+            return Search_Recursive(A, key, p, mid-1);
+    }
 
     public static void main(String[] args) {
         int[] A = {1, 2, 3, 4, 5, 6};
@@ -20,14 +30,14 @@ public class Binary_Search {
         
         int id = Search_Iterative(A, key, p, r);
         if (id == -1)
-            System.out.println("Element " + key + " not found in the array!");
+            System.out.println("(Iterative) Element " + key + " not found in the array!");
         else
-            System.out.println("Element " + key + " found at index: " + id);
+            System.out.println("(Iterative) Element " + key + " found at index: " + id);
         
         int idx = Search_Recursive(A, key, p, r);
         if (idx == -1)
-            System.out.println("Element " + key + " not found in the array!");
+            System.out.println("(Recursive) Element " + key + " not found in the array!");
         else
-            System.out.println("Element " + key + " found at index: " + idx);
+            System.out.println("(Recursive) Element " + key + " found at index: " + idx);
     }
 }
