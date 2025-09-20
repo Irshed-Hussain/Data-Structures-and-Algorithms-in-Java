d954065da388037859b99a6e46dff0c34b4b5300
Dr. Irshed Hussain <70743676+Irshed-Hussain@users.noreply.github.com>
2025-09-20 13:30:45 +0530

    Update Factorial_Number.java

--- iteration_recursion/Factorial_Number.java
+++ iteration_recursion/Factorial_Number.java
@@ -1,1 +1,21 @@
+public class Factorial_Number {
+    
+    public static int Fact_Iterative(int n) {
+        int fact = 1;
+        for (int i=1; i<=n; i++)
+            fact *= i;  
+        return fact;
+    }
 
+    public static int Fact_Recursive(int n) {
+        if (n == 0) 
+            return 1; 
+        return n * Fact_Recursive(n - 1); 
+    }
+
+    public static void main(String[] args) {
+        int n = 4; 
+        System.out.println("(Iterative) Factorial of " + n + " is: " + Fact_Iterative(n));
+        System.out.println("(Recursive) Factorial of " + n + " is: " + Fact_Recursive(n));
+    }
+}
