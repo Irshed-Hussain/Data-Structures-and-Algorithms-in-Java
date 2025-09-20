1d929f99e46e0bbbb112f727dc0587cc917d4975
Dr. Irshed Hussain <70743676+Irshed-Hussain@users.noreply.github.com>
2025-09-20 13:20:03 +0530

    Update and rename Linear_Search_Iterative.java to Linear_Search.java

--- iteration_recursion/Linear_Search.java
+++ iteration_recursion/Linear_Search.java
@@ -1,5 +1,5 @@
-public class Linear_Search_Iterative {
-    public static int search_algo(int[] A, int key, int idx) {
+public class Linear_Search {
+    public static int Search_Iterative(int[] A, int key, int idx) {
        for(int i=0; i<A.length-1; i++) {
             if(A[i] == key)
                 return i;
@@ -7,10 +7,19 @@ public class Linear_Search_Iterative {
        return -1;
    }
 
+    
+
    public static void main(String[] args) {
        int[] A = {6, 2, 4, 5, 1, 3};
        int key = 5;
-       int idx = search_algo(A, key, 0);
+       
+       int id = Search_Iterative(A, key, 0);
+       if (id == -1)
+           System.out.println("Element " + key + " not found in the array!");
+       else
+           System.out.println("Element " + key + " found at index: " + id);
+        
+       int idx = Search_Recursive(A, key, 0);
        if (idx == -1)
            System.out.println("Element " + key + " not found in the array!");
        else
