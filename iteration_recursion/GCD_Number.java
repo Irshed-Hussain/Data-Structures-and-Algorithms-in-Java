847da141db00dba2991e39c9da44ecb050c392d9
Dr. Irshed Hussain <70743676+Irshed-Hussain@users.noreply.github.com>
2025-09-29 21:35:16 +0530

    Update GCD_Number.java

--- iteration_recursion/GCD_Number.java
+++ iteration_recursion/GCD_Number.java
@@ -1,1 +1,25 @@
+public class GCD_Number {
+    
+    public static int Gcd_Iterative(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
 
+    public static int Gcd_Recursive(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return Gcd_Recursive(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        int a = 48;
+        int b = 18;
+        System.out.println("(Iterative) GCD of " + a + " and " + b + " is: " + Gcd_Iterative(a, b));
+        System.out.println("(Recursive) GCD of " + a + " and " + b + " is: " + Gcd_Recursive(a, b));
+    }
+}
