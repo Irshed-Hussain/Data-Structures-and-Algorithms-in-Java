bbb62f6b79e5b4c49c5eee2628db438d178ec4de
Dr. Irshed Hussain <70743676+Irshed-Hussain@users.noreply.github.com>
2025-09-29 21:18:11 +0530

    Update Integer_Hex.java

--- iteration_recursion/Integer_Hex.java
+++ iteration_recursion/Integer_Hex.java
@@ -1,1 +1,29 @@
+public class Integer_Hex {
 
+    public static String Int_to_Hex(int n) {
+        if (n == 0)
+            return " ";
+
+        int rem = n % 16;
+        char hex;
+
+        if (rem < 10)
+            hex = (char) ('0' + rem);
+        else
+            hex = (char) ('A' + (rem - 10));
+
+        return Int_to_Hex(n / 16) + hex;
+    }
+
+    public static void main(String[] args) {
+        int dec = 14; 
+
+        if (dec == 0) 
+            System.out.println("Hexadecimal: 0");
+        else {
+            String hex = Int_to_Hex(dec);
+            System.out.println("Decimal: " + dec);
+            System.out.println("Hexadecimal: " + hex);
+        }
+    }
+}
